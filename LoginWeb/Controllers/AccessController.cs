4e892c5c1bb8cbf82f783cc3b9eb807b9d93d33f
Esther Yap <esther_yap93@yahoo.com>
2022-06-15 16:14:07 +0800

    Add project files.

---
+++ LoginWeb/Controllers/AccessController.cs
@@ -0,0 +1,37 @@
+﻿using LoginWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace LoginWeb.Controllers
+{
+    public class AccessController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginViewModel model)
+        {
+
+            if (ModelState.IsValid == true)
+            {
+
+                if (model.Username == "User" && model.Password == "Pa$$W0rd")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+
+                    ViewBag.Message = "Invalid UserName or password";
+                    return View(model);
+                }
+                ModelState.AddModelError("", "Invalid username/ password.");
+            }
+            return View(model);
+        }
+
+    }
+}
